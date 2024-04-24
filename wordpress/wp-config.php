a69f0d4bdd502119a389277b7f69e6206160999c
santiagord2004 <99020026+santiagord2004@users.noreply.github.com>
2024-04-24 13:44:10 -0500

    wp-config.php

---
+++ wordpress/wp-config.php
@@ -0,0 +1,7 @@
+<?php
+define( 'DB_NAME', getenv('wordpress') );
+define( 'DB_USER', getenv('wordpress') );
+define( 'DB_PASSWORD', getenv('123') );
+define( 'DB_HOST', getenv('172.31.27.136') );
+define( 'DB_CHARSET', 'utf8' );
+define( 'DB_COLLATE', '' );
